473680364e5c514ed0e8744eca81c4109c5775e6
Rachel D. Cartwright <stormchica@gmail.com>
2024-09-02 17:15:23 -0500

    some cleanup

--- client_new/src/components/BenefitModal.tsx
+++ client_new/src/components/BenefitModal.tsx
@@ -1,93 +1,55 @@
 import { useForm } from '@tanstack/react-form'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import {
-  FormControl,
-  FormLabel,
-  FormHelperText,
-  Input, 
   Button,
 } from '@chakra-ui/react'
 import { createBenefit } from '../api/index'
 import Modal from './Modal';
+import ItemForm from './ItemForm';
 
 
-const Form = ({ form } : {form: any}) => {
-  return (
-    <form>
-      <form.Field
-        name="description"
-        children={(field) => (
-          <FormControl>
-            <FormLabel>Description</FormLabel>
-            <Input
-              name={field.name}
-              value={field.state.value}
-              onBlur={field.handleBlur}
-              onChange={(e) => field.handleChange(e.target.value)}
-            />
-          </FormControl>
-        )}
-      />
-      <form.Field
-        name="weight"
-        children={(field) => (
-          <FormControl>
-            <FormLabel>Weight</FormLabel>
-            <Input
-              name={field.name}
-              value={field.state.value}
-              onBlur={field.handleBlur}
-              onChange={(e) => field.handleChange(e.target.value)}
-            />
-            <FormHelperText>How important is this from a 1 - 10 scale with 10 being the most important</FormHelperText>
-          </FormControl>
-        )}
-      />
-    </form>
-  )
-}
 export function NewBenefit({isOpen, onClose}: {isOpen: boolean, onClose: () => void}) {
   const queryClient = useQueryClient()
 
   const mutation = useMutation({
-      mutationFn: createBenefit,
-      onSuccess: () => {
-        // Invalidate and refetch
-        queryClient.invalidateQueries({ queryKey: ['benefits'] })
-      },
-    })
+    mutationFn: createBenefit,
+    onSuccess: () => {
+      // Invalidate and refetch
+      queryClient.invalidateQueries({ queryKey: ['benefits'] });
+      onClose();
+    },
+  });
     
   const form = useForm({
-      defaultValues: {
-        description: '',
-        weight: '',
-        analysisId: ''
-      },
-      onSubmit: async ({ value }) => {
-        // Do something with form data
-        console.log(value)
-        mutation.mutate({
-          description: value.description,
-          weight: Number(value.weight),
-          analysis_id: 2
-        })
-      },
-  })
+    defaultValues: {
+      description: '',
+      weight: 1,
+    },
+    onSubmit: (values) => {
+      mutation.mutate({
+        ...values,
+        analysis_id: 2,
+      });
+    },
+  });
+
 
   return (
     <Modal
       isOpen={isOpen} 
       onClose={onClose}
       title={`Add ${name}`} 
-      content={<Form form={form} />}
+      content={<ItemForm form={form} />}
       footer={
         <form.Subscribe
           selector={(state) => [state.isSubmitting]}
-          children={([isSubmitting]) => (
+          children={([isSubmitting]) => {
+            return (
             <Button colorScheme="blue" mr={3} onClick={() => form.handleSubmit()} isLoading={isSubmitting}>
               Save
             </Button>
-          )}
+          )
+          }}
         />
       }
     />
---
+++ client_new/src/components/ItemForm.tsx
@@ -0,0 +1,41 @@
+import { FormControl, FormHelperText, FormLabel, Input } from "@chakra-ui/react";
+
+
+const ItemForm = ({ form } : {form: any}) => {
+  return (
+    <form>
+      <form.Field
+        name="description"
+        children={(field) => (
+          <FormControl>
+            <FormLabel>Description</FormLabel>
+            <Input
+              name={field.name}
+              value={field.state.value}
+              onBlur={field.handleBlur}
+              onChange={(e) => field.handleChange(e.target.value)}
+            />
+          </FormControl>
+        )}
+      />
+      <form.Field
+        name="weight"
+        children={(field) => (
+          <FormControl>
+            <FormLabel>Weight</FormLabel>
+            <Input
+              name={field.name}
+              value={field.state.value}
+              onBlur={field.handleBlur}
+              onChange={(e) => field.handleChange(e.target.value)}
+            />
+            <FormHelperText>How important is this from a 1 - 10 scale with 10 being the most important</FormHelperText>
+          </FormControl>
+        )}
+      />
+    </form>
+  )
+}
+
+
+export default ItemForm
