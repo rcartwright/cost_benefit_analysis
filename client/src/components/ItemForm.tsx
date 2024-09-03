aaeedeb10a1565673b7046423428575fa7fb0013
Rachel D. Cartwright <stormchica@gmail.com>
2024-09-02 23:56:37 -0500

    add form validation

--- client/src/components/ItemForm.tsx
+++ client/src/components/ItemForm.tsx
@@ -1,13 +1,44 @@
-import { FormControl, FormHelperText, FormLabel, Input } from "@chakra-ui/react";
+import { 
+  FormControl, 
+  FormHelperText, 
+  FormLabel, 
+  Input, 
+  FormErrorMessage, 
+  NumberInput, 
+  NumberInputField, 
+  NumberInputStepper, 
+  NumberIncrementStepper, 
+  NumberDecrementStepper 
+} from "@chakra-ui/react";
+import { FieldApi } from '@tanstack/react-form';
 
-
+function FieldInfo({ field }: { field: FieldApi<any, any, any, any> }) {
+  return (
+    <>
+      {field.state.meta.isTouched && field.state.meta.errors.length ? (
+        <FormErrorMessage>
+          <em>{field.state.meta.errors.join(',')}</em>
+        </FormErrorMessage>
+      ) : null}
+      {field.state.meta.isValidating ? 'Validating...' : null}
+    </>
+  )
+}
 const ItemForm = ({ form } : {form: any}) => {
   return (
     <form>
       <form.Field
         name="description"
+        validators={{
+          onChange: ({ value }) =>
+            !value
+              ? 'A description is required'
+              : value.length < 3
+                ? 'Description must be at least 3 characters'
+                : undefined,
+        }}
         children={(field: any) => (
-          <FormControl>
+          <FormControl className="pb-2" isInvalid={field.state.meta.isTouched && !!field.state.meta.errors.length}>
             <FormLabel>Description</FormLabel>
             <Input
               name={field.name}
@@ -15,20 +46,36 @@ const ItemForm = ({ form } : {form: any}) => {
               onBlur={field.handleBlur}
               onChange={(e) => field.handleChange(e.target.value)}
             />
+            <FieldInfo field={field} />
           </FormControl>
         )}
       />
       <form.Field
         name="weight"
+        validators={{
+          onChange: ({ value }) =>
+            !value
+              ? 'A weight is required'
+              : (Number(value) < 1 || Number(value) > 10)
+                ? 'Weight must be between 1 and 10'
+                : undefined,
+        }}
         children={(field: any) => (
-          <FormControl>
+          <FormControl className="pb-2" isInvalid={field.state.meta.isTouched && !!field.state.meta.errors.length}>
             <FormLabel>Weight</FormLabel>
-            <Input
-              name={field.name}
-              value={field.state.value}
-              onBlur={field.handleBlur}
-              onChange={(e) => field.handleChange(e.target.value)}
-            />
+            <NumberInput min={1} max={10} keepWithinRange>
+              <NumberInputField 
+                name={field.name}
+                value={field.state.value}
+                onBlur={field.handleBlur}
+                onChange={(e) => field.handleChange(e.target.value)} 
+              />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+            <FieldInfo field={field} />
             <FormHelperText>How important is this from a 1 - 10 scale with 10 being the most important</FormHelperText>
           </FormControl>
         )}
